036dfb435998cea228c2b29b270ccd6935a8fdb3
Kingma <e_kingma@ats-hr420-dm20.fc.ca>
2020-01-17 11:07:59 -0500

    adding js

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@
 
 (() => {
   console.log('hello from JS!');
-
+//find the elements in the document
   let toggleButton = document.querySelector(".switch-text"),
     myHeading = document.querySelector('h1'),
-    svgImg = document.querySelector("#badge");
-
+    //svgImg = document.querySelector("#badge"),
+    allSVGs = document.querySelectorAll(".svg");
+//this function should change the heading text
   function changeText() {
-    myHeading.textContent = "What is up? I am Script!"
+    myHeading.textContent = "What is up? I am Script!";
   }
+  // this function should log the id for each svg to the console
   function logSVG(){
     console.log(this.id);
   }
+  //things a user can do to make things happen on the page
   toggleButton.addEventListener("click", changeText);
-  svgImg.addEventListener("mouseover", logSVG);
+
+  //svgImg.addEventListener("click", logSVG);
+  //svgImg2.addEventListener("click", logSVG);
+
+  allSVGs.forEach(item => item.addEventListener("click", logSVG));
   //document.querySelector('h1').textContent = "What is up? I am Script!"
 
 })();
